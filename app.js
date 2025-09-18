9f9bf1bcd199da10974090b56a919126b79dd76a
M@tinGG <m.ghafari904@gmail.com>
2025-09-18 10:15:45 +0330

    nonononononono

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+import TelegramBot from "node-telegram-bot-api";
 import dotenv from 'dotenv'
 dotenv.config()
+const TOKEN = process.env.BOT_TOKEN;
+const bot = new TelegramBot(TOKEN,{
+    baseApiUrl:"https://tapi.bale.ai",
+    polling: true,
+})
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, "تو هم همین طور");
+});
